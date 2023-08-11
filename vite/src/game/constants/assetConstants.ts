51fb2b75b38729ede9a0b05ac0b0575cb9dba482
bednyakov <bednyakov99@gmail.com>
2023-08-11 09:47:32 +0300

    change cells style manually #7

--- vite/src/game/constants/assetConstants.ts
+++ vite/src/game/constants/assetConstants.ts
@@ -1,11 +1,11 @@
 export const SPRITES = {
     CELL: {
         KEY: 'cell',
-        PATH: 'assets/img/cell.png',
+        PATH: 'assets/img/cells.png',
         HOVER_ZOOM: 4,
         HOVER: {
             KEY: 'cellHover',
-            PATH: 'assets/img/cellHover.png',
+            PATH: 'assets/img/cellsHover.png',
         }
     },
 };
